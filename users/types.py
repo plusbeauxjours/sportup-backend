8f32700dbec7c3e395a7bf2d5069f9d19bf873ab
plusbeauxjours <plusbeauxjours@gmail.com>
2020-03-25 19:10:13 +0900

    users.types

--- users/types.py
+++ users/types.py
@@ -1,14 +1,68 @@
 import graphene
+from . import models
 from django.contrib.auth import get_user_model
 from graphene_django.types import DjangoObjectType
+from django.core.paginator import Paginator
+from graphql_jwt.decorators import login_required
+
+
+class UserPlaysSportType(DjangoObjectType):
+    sport_id = graphene.Int()
+    name = graphene.String()
+    img_url = graphene.String()
+
+    class Meta:
+        model = models.UserPlaysSport
+
+    def resolve_sport_id(self, info):
+        return self.sport.id
+
+    def resolve_name(self, info):
+        return self.sport.name
+
+    def resolve_img_url(self, info):
+        return self.sport.img_path
 
 
 class UserType(DjangoObjectType):
+    sports = graphene.List(UserPlaysSportType)
+    following_count = graphene.Int()
+    followers_count = graphene.Int()
+    is_following = graphene.Boolean()
+
     class Meta:
         model = get_user_model()
 
-    def resolve_name(self, info):
-        return self.get_full_name()
+    def resolve_sports(self, info):
+        return self.userplayssport_set.all()
+
+    def resolve_followers(self, info, page_num=1):
+        qs = models.User.objects.filter(profile__in=self.user.followers.all())
+        pg = Paginator(qs, 12)
+        return pg.get_page(page_num)
+
+    def resolve_following(self, info, page_num=1):
+        qs = self.following.all()
+        pg = Paginator(qs, 12)
+        return pg.get_page(page_num)
+
+    def resolve_following_count(self, info):
+        return self.following.count()
+
+    def resolve_followers_count(self, info):
+        return self.user.followers.count()
+
+    @login_required
+    def resolve_is_following(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Not authenticated!")
+
+        try:
+            f = user.following.get(pk=self.pkd)
+            return True
+        except models.ObjectDoesNotExist:
+            return False
 
 
 class MeReponse(graphene.ObjectType):
