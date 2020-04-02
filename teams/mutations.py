03ec8dcf6c0c79187c1e8e1e03a0270ea865fc78
plusbeauxjours <plusbeauxjours@gmail.com>
2020-04-02 15:22:37 +0900

    createTeam Done

--- teams/mutations.py
+++ teams/mutations.py
@@ -1,31 +1,31 @@
 import graphene
 from graphql_jwt.decorators import login_required
 from users import models as user_models
-from sport import models as sport_models
+from sports import models as sport_models
 from . import types, models
 
 
 class CreateTeam(graphene.Mutation):
     class Arguments:
         name = graphene.String(required=True)
-        sport_id = graphene.Int(rewquired=True)
-        member_ids = graphene.List(graphene.String)
+        sport_id = graphene.Int(required=True)
+        member_uuids = graphene.List(graphene.String)
 
     Output = types.CreateTeamResponse
 
     @login_required
     def mutate(self, info, **kwargs):
         user = info.context.user
-        sport_id = kwargs.get("sport_id")
         name = kwargs.get("name")
-        member_ids = kwargs.get("member_ids")
+        sport_id = kwargs.get("sport_id")
+        member_uuids = kwargs.get("member_uuids")
 
         try:
             sport = sport_models.Sport.objects.get(pk=sport_id)
-            team = models.Team.objects.create(name=name, sport=sport, create_by=user)
+            team = models.Team.objects.create(name=name, sport=sport, created_by=user)
             tm = models.TeamMember.objects.create(user=user, team=team, is_admin=True)
 
-            new_members = user_models.User.objects.filter(pk__in=member_ids)
+            new_members = user_models.User.objects.filter(uuid__in=member_uuids)
             for member in new_members:
                 if member.uuid != user.uuid:
                     models.TeamMember.objects.create(user=member, team=team)
@@ -69,3 +69,15 @@ class AddTeamMember(graphene.Mutation):
 
         except models.Team.DoesNotExist:
             return types.AddTeamMemberResponse(ok=False)
+
+
+# class RemoveTeamMember(graphene.Mutation):
+#     class Arguments:
+#         team_id = graphene.Int()
+#         uuid = graphene.String()
+
+#     @login_required
+#     def mutate(self, info, team_id, user_id):
+#         user = info.context.user
+
+#         team = models.Team
