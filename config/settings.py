90f9d931cf499de894f83fcb3fdb820fdc32992b
plusbeauxjours <plusbeauxjours@gmail.com>
2020-07-18 14:50:55 +0700

    SET debug=False

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "&k^q=t5ik+ttfdfruh5wv%($k$&)46%q7kirwf(03nx0ry6%w+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
