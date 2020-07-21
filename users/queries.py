f30da464712bc3bba81c8389c622f48ff108ed0d
plusbeauxjours <plusbeauxjours@gmail.com>
2020-07-21 23:37:28 +0700

    SET permission for me query

--- users/queries.py
+++ users/queries.py
@@ -3,7 +3,6 @@ from django.db.models import Q
 from graphql_jwt.decorators import login_required
 
 
-@login_required
 def resolve_me(self, info):
     user = info.context.user
     return types.MeReponse(user=user)
